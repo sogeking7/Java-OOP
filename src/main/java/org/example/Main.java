c903c6b732ee87054bf49184e5a04277dba4a8b4
Kaiyrkhan <kairolla222@gmail.com>
2025-04-05 03:14:16 +0500

    getters and setters - Encapsulation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        User u = new User();
+        u.set_name("Alex");
+        u.membership = "Gold";
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        User u2 = new User();
+        u2.set_name("Tom");
+        u2.membership = "Premium";
+
+        System.out.println(u.get_name());
+        System.out.println(u2.get_name());
     }
 }
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class User {
+
+    private String _name;
+    String membership;
+
+    // Encapsulation set method
+    void set_name(String name) {
+        _name = name;
+    }
+
+    // Encapsulation get method
+    String get_name() {
+        return _name;
+    }
+}
