30c893ba324ec9ee8734c54d58aaa4abdafb0c36
Kaiyrkhan <kairolla222@gmail.com>
2025-04-05 03:22:23 +0500

    Method Overloading

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,13 @@ public class Main {
         System.out.println("Hello and welcome!");
         User u = new User();
         u.set_name("Alex");
-        u.membership = "Gold";
+        u.set_name("Gold");
 
         User u2 = new User();
         u2.set_name("Tom");
-        u2.membership = "Premium";
+        u2.set_membership(User.Membership.Gold);
 
         System.out.println(u.get_name());
-        System.out.println(u2.get_name());
+        System.out.println(u2.get_name() + " " + u2.get_membership());
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,7 +3,7 @@ package org.example;
 public class User {
 
     private String _name;
-    String membership;
+    private String _membership;
 
     // Encapsulation set method
     void set_name(String name) {
@@ -13,5 +13,25 @@ public class User {
     // Encapsulation get method
     String get_name() {
         return _name;
+    }
+
+    // Encapsulation set method
+    void set_membership(String membership) {
+        _membership = membership;
+    }
+
+    void set_membership(Membership membership) {
+        _membership = membership.name();
+    }
+
+    public enum Membership {
+        Bronze,
+        Silver,
+        Gold,
+    }
+
+    // Encapsulation get method
+    String get_membership() {
+        return _membership;
     }
 }
